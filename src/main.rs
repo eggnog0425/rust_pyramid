9eacb88717b3753fa0b2cc0e3c8d1a86cc0afb3e
eggnog0425 <haruyo.coffee@gmail.com>
2020-06-17 20:21:00 +0900

    入力値が255の場合にmaxの値がu8の最大値より大きくなるため生じるエラーを修正し、testableなコードにリファクタ

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,47 @@ fn main() {
     let steps = read_steps();
 
     println!("your pyramid steps is {}.", steps);
-    // ここからピラミッド建立ゾーン
+    build_pyramid(steps);
+}
+
+fn read_steps() -> u16 {
+    loop {
+        let mut steps = String::new();
+        let input = io::stdin().read_line(&mut steps);
+        if valid_input(&input, &steps) {
+            return steps.trim().parse().unwrap();
+        }
+        continue;
+    }
+}
+
+fn valid_input(input: &io::Result<usize>, steps: &String) -> bool {
+    if input.is_err() {
+        println!("Wwops, failed to read line. please re-enter.");
+        return false;
+    }
+
+    let steps: Result<u16, std::num::ParseIntError> = steps.trim().parse();
+
+    match steps {
+        Ok(_v) => {
+            let i = steps.unwrap();
+            if 0 < i && i < 256 {
+                return true;
+            }
+            println!("Please re-enter number(1-255).");
+            return false;
+        }
+        Err(_e) => {
+            println!("Please re-enter number(1-255).");
+            return false;
+        }
+    }
+}
+
+fn build_pyramid(steps: u16) {
     let max = steps + 1;
+
     for i in 1..max {
         let brock = &String::from("■").repeat(i.into());
         let space = String::from(" ").repeat((max - i - 1).into());
@@ -15,22 +54,14 @@ fn main() {
     }
 }
 
-// 関数に<'a>(生存期間パラメータ)をつけてあげて、&strも関数と同じだけ生きることを明示する
-fn read_steps() -> u8 {
-    loop {
-        let mut steps = String::new();
-        if io::stdin().read_line(&mut steps).is_err() {
-            println!("Wwops, failed to read line. please re-enter.");
-            continue;
-        }
-
-        let steps: Result<u8, std::num::ParseIntError> = steps.trim().parse();
-
-        // steps.unwrap()だとResultの参照が死ぬのでas_ref()で参照型として取得した上で参照外し演算子*で参照先の値を取りにいく
-        if steps.is_err() || *steps.as_ref().unwrap() == 0 {
-            println!("Please re-enter number(1-255).");
-            continue;
-        }
-        return steps.unwrap();
-    }
+#[cfg(test)]
+fn test_read_steps() {
+    // 空文字
+    // 半角スペース
+    // 2byte文字列
+    // -1
+    // 0
+    // 1
+    // 255
+    // 256
 }
